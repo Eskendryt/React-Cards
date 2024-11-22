163012db147b458320f091877c8a6ebcd1157da4
Eskndr <eskndrytbarek323@gmail.com>
2024-11-22 10:30:03 +0300

    completed

---
+++ src/Card.jsx
@@ -0,0 +1,22 @@
+import mypic from './assets/th.jpeg'
+
+function Card(props){
+return(
+   <div className="card">
+
+   <img src={mypic} alt="my image" />
+   <h2>Name: {props.Name}</h2>
+   <p>Age; {props.Age}</p>
+   <p>Enrolled: {props.isEnrolled ? "Yes": "NO" } </p>
+   </div>
+);
+}
+
+
+Card.defaultProps={
+    Name:"Guest",
+    Age:0,
+    isStudent:false
+}
+
+export default Card
